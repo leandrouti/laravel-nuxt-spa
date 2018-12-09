a9991147f017f3e1477114c49f737bb0294d0525
Utiyama Tatsuya Leandro <leandrouti@yahoo.com.br>
2018-12-09 21:08:21 +0900

    modified index

---
+++ resources/nuxt/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hello {{ user.name }}!</h1>
+    <h2>Your age is {{ user.age }}</h2>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data: () => {
+    return { name: 'bal' };
+  },
+
+  async asyncData({app}){
+    const user = await app.$axios.$get('api/me');
+
+    console.log(user)
+    return {user};
+  },
+};
+</script>
